cbab646ab0e04ce8aaa82b550a0e8fccfdb12526
Jong Park <mikepark@mikepark-imac.local>
2019-04-13 16:53:49 +0900

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jong tae Park on 13/04/2019.
+//  Copyright © 2019 Jong tae Park. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
